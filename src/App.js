51178d73ebfb933636b57d5ddfd18e3ac1ba2667
David Maser <davidemaser@gmail.com>
2017-04-27 10:57:41 -0400

    Remove unused Components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Content from "./Components/Content";
 import Footer from "./Components/Footer";
 import JsonData from "./Data/Schema/page.json"; //load the page schema directly and pass it around the components
 import Helmet from 'react-helmet';
-import UIDrawer from "./Components/UI/Drawer";
 import ActionButton from "./Components/UI/ActionButton";
-import Snackbar from 'material-ui/Snackbar';
-import DoSomeMath from "./Components/Renderers/FormatMath";
 import TableView from "./Components/Renderers/TableView";
 import LoadJson from "./Components/Loaders/JsonLoader";
 import DataList from './Data/Table.json';
 import BoxMarquee from "./Components/UI/BoxMarquee";
-import Loading from "./Components/UI/Loading";
 import BlockView from "./Components/UI/BlockView";
 import FlexBlockView from "./Components/UI/FlexBlockView";
 import PropTypes from 'prop-types';
@@ -48,8 +44,6 @@ class App extends Component {
                 <BlockView color={1} background={3}><p>This would be content</p></BlockView>
                 <BlockView color={7} background={5}><p>This would be content</p></BlockView>
                 <BlockView color={7} background={2}><p>This would be content</p></BlockView>
-                {/*<Loading background="rgba(0, 0, 0, 0.85)" color="#fff" show={true} error={true} />
-                <UIDrawer/>*/}
                 <Helmet>
                     <meta charSet="utf-8" />
                     <title>{this.state.data.title}</title>
@@ -60,8 +54,6 @@ class App extends Component {
                 <Content data={this.state.data} node="body"/>
                 <Footer data={this.state.data} node="footer"/>
                 <ActionButton/>
-                {/*<Snackbar open={this.state.snackbarOpen} message={this.state.snackbarMessage} autoHideDuration={5000} />
-                <DoSomeMath a="8" b="20" method="divi" operand="none" />*/}
                 <BoxMarquee title="this would be the title" content="this is a title"/>
                 <TableView data={DataList['table']} />
                 <LoadJson url="http://davidemaser.github.io/data/temp-table.json" method="get" target="table"/>
